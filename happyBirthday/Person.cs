26f35071167ec4aca41d218f445efa3ef1cb329f
im <idevm5@ya.ru>
2021-10-10 23:44:31 +0300

    class person added

---
+++ happyBirthday/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace happyBirthday
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        private string name;
+        private int birthday;
+        private int birthmonth;
+        private int birthyear;
+
+        public string Name { get => name; set => name = value; }
+
+        public int Birthday { get => birthday; set => birthday = value; }
+
+        public int Birthmonth { get => birthmonth; set => birthmonth = value; }
+
+        public int Birthyear { get => birthyear; set => birthyear = value; }
+
+        public int number;
+
+
+        public string GetAge(int yearNow)
+        {
+            int age = yearNow - Birthyear;
+            string str = age.ToString();
+            if (age % 5 == 0 && age >= 50)
+            {
+                str = "юбилей: " + str;
+            }
+            if ((str.EndsWith('2') || str.EndsWith('3') || str.EndsWith('4')) && (!str.StartsWith('1')
+                || str.Length == 3))
+            {
+                str += " года";
+            }
+            else if (str.EndsWith('1') && (!str.StartsWith('1') || str.Length == 1 || str.Length == 3))
+            {
+                str += " год";
+            }
+            else
+            {
+                str += " лет";
+            }
+            return str;
+        }
+    }
+}
