6992c4643d37ff3165968b20de380a9b39b48cb2
im <idevm5@ya.ru>
2021-10-11 23:11:35 +0300

    added variants of use in console

--- happyBirthday/Program.cs
+++ happyBirthday/Program.cs
@@ -19,15 +19,41 @@ namespace happyBirthday
                 //thisYear
                 app.Text = app.GetText(app.PeopleListFilter(app.Persons, TimeMode.thisYear), TimeMode.thisYear);
                 app.ShowText(app.Text);
-                //createNewBirthDay
-                app.Persons = app.AddPerson(app.Persons);
-                app.WriteFile("db.csv", app.AddText(app.Persons));
+                //create person or delete
+                while (true)
+                {
+                    app.ShowText($"[c]reate, [r]emove, [s]how or [q]uit? [enter]");
+                    string answer = HBApp.GetTextFromUser();
+                    if (answer == "c")
+                    {
+                        app.Persons = app.AddPerson(app.Persons);
+                        app.WriteFile("db.csv", app.UpdateText(app.Persons));
+                    }
+                    else if (answer == "r")
+                    {
+                        app.Persons = app.RemovePerson(app.Persons);
+                        app.WriteFile("db.csv", app.UpdateText(app.Persons));
+                    }
+                    else if (answer == "s")
+                    {
+                        app.Text = app.GetText(app.PeopleListFilter(app.Persons, TimeMode.thisYear), TimeMode.thisYear);
+                        app.ShowText(app.Text);
+                    }
+                    else if (answer == "q")
+                    {
+                        break;
+                    }
+                }
             }
             catch (System.IO.FileNotFoundException ex)
             {
                 app.ShowText(ex.Message);
             }
             catch (FormatException ex)
+            {
+                app.ShowText(ex.Message);
+            }
+            catch (ArgumentException ex)
             {
                 app.ShowText(ex.Message);
             }
