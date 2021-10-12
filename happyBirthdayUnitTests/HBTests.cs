9dcc8e34f334aeabeec3295c52521137e4ccb39b
im <idevm5@ya.ru>
2021-10-12 23:25:18 +0300

    formatting code

--- happyBirthdayUnitTests/HBTests.cs
+++ happyBirthdayUnitTests/HBTests.cs
@@ -21,8 +21,12 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("db")][TestCase("db.")][TestCase("d.csv")]
-        [TestCase("db.xls")][TestCase("db.txt")][TestCase("../db.csv")]
+        [TestCase("db")]
+        [TestCase("db.")]
+        [TestCase("d.csv")]
+        [TestCase("db.xls")]
+        [TestCase("db.txt")]
+        [TestCase("../db.csv")]
         public void ReadFile_BadFiles_ThrowException(string path)
         {
             Exception ex = Assert.Catch(() => app.ReadFile(path));
@@ -108,7 +112,7 @@ namespace happyBirthdayUnitTests
                     Birthyear = 1991
                 }
             };
-            List <Person> exp = new();
+            List<Person> exp = new();
             List<Person> res = app.PeopleListFilter(people);
             Assert.AreEqual(exp, res);
         }
@@ -148,7 +152,7 @@ namespace happyBirthdayUnitTests
             {
                 exp.Add(people[i]);
             }
-            List<Person> res = app.PeopleListFilter(people,tm);
+            List<Person> res = app.PeopleListFilter(people, tm);
             Assert.AreEqual(exp, res);
         }
 
@@ -171,9 +175,14 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("1;;Ivan;;01.01.1990")][TestCase("1;;Ivan;;")]
-        [TestCase("1;;Ivan;")][TestCase("1;;Ivan")][TestCase("1;;")]
-        [TestCase("1;")][TestCase("1")][TestCase("")]
+        [TestCase("1;;Ivan;;01.01.1990")]
+        [TestCase("1;;Ivan;;")]
+        [TestCase("1;;Ivan;")]
+        [TestCase("1;;Ivan")]
+        [TestCase("1;;")]
+        [TestCase("1;")]
+        [TestCase("1")]
+        [TestCase("")]
         public void GetPeopleList_BadFormatting_ThrowsExceptiion(string line)
         {
             List<string> lines = new() { line };
@@ -191,8 +200,10 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("1;;Ivan;;.01.1990;")][TestCase("1;;Ivan;;001.01.1990;")]
-        [TestCase("1;;Ivan;;f.01.1990;")][TestCase("1;;Ivan;;33.01.1990;")]
+        [TestCase("1;;Ivan;;.01.1990;")]
+        [TestCase("1;;Ivan;;001.01.1990;")]
+        [TestCase("1;;Ivan;;f.01.1990;")]
+        [TestCase("1;;Ivan;;33.01.1990;")]
         public void GetPeopleList_BadDay_ThrowsExceptiion(string line)
         {
             List<string> lines = new() { line };
@@ -201,8 +212,10 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("1;;Ivan;;01..1990;")][TestCase("1;;Ivan;;01.010.1990;")]
-        [TestCase("1;;Ivan;;01.h.1990;")][TestCase("1;;Ivan;;01.22.1990;")]
+        [TestCase("1;;Ivan;;01..1990;")]
+        [TestCase("1;;Ivan;;01.010.1990;")]
+        [TestCase("1;;Ivan;;01.h.1990;")]
+        [TestCase("1;;Ivan;;01.22.1990;")]
         public void GetPeopleList_BadMonth_ThrowsExceptiion(string line)
         {
             List<string> lines = new() { line };
@@ -211,8 +224,10 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("1;;Ivan;;01.01.199;")][TestCase("1;;Ivan;;01.01.19902;")]
-        [TestCase("1;;Ivan;;01.01.d;")][TestCase("1;;Ivan;;01.01.;")]
+        [TestCase("1;;Ivan;;01.01.199;")]
+        [TestCase("1;;Ivan;;01.01.19902;")]
+        [TestCase("1;;Ivan;;01.01.d;")]
+        [TestCase("1;;Ivan;;01.01.;")]
         public void GetPeopleList_BadYear_ThrowsExceptiion(string line)
         {
             List<string> lines = new() { line };
@@ -261,7 +276,7 @@ namespace happyBirthdayUnitTests
                     Birthmonth = ThisMonth,
                     Birthyear = 1990
                 },
-                new Person ("Iva")
+                new Person("Iva")
                 {
                     Birthday = ThisDay,
                     Birthmonth = ThisMonth,
@@ -383,20 +398,48 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase(0, "0 лет")][TestCase(1, "1 год")][TestCase(2, "2 года")]
-        [TestCase(3, "3 года")][TestCase(4, "4 года")][TestCase(5, "5 лет")]
-        [TestCase(6, "6 лет")][TestCase(7, "7 лет")][TestCase(8, "8 лет")]
-        [TestCase(9, "9 лет")][TestCase(10, "10 лет")][TestCase(11, "11 лет")]
-        [TestCase(12, "12 лет")][TestCase(13, "13 лет")][TestCase(14, "14 лет")]
-        [TestCase(15, "15 лет")][TestCase(16, "16 лет")][TestCase(17, "17 лет")]
-        [TestCase(18, "18 лет")][TestCase(19, "19 лет")][TestCase(20, "20 лет")]
-        [TestCase(21, "21 год")][TestCase(22, "22 года")][TestCase(23, "23 года")]
-        [TestCase(24, "24 года")][TestCase(25, "25 лет")][TestCase(30, "30 лет")]
-        [TestCase(33, "33 года")][TestCase(35, "35 лет")][TestCase(49, "49 лет")]
-        [TestCase(50, "юбилей: 50 лет")][TestCase(51, "51 год")][TestCase(55, "юбилей: 55 лет")]
-        [TestCase(80, "юбилей: 80 лет")][TestCase(81, "81 год")][TestCase(100, "юбилей: 100 лет")]
-        [TestCase(101, "101 год")][TestCase(102, "102 года")][TestCase(103, "103 года")]
-        [TestCase(105, "юбилей: 105 лет")][TestCase(205, "юбилей: 205 лет")][TestCase(301, "301 год")]
+        [TestCase(0, "0 лет")]
+        [TestCase(1, "1 год")]
+        [TestCase(2, "2 года")]
+        [TestCase(3, "3 года")]
+        [TestCase(4, "4 года")]
+        [TestCase(5, "5 лет")]
+        [TestCase(6, "6 лет")]
+        [TestCase(7, "7 лет")]
+        [TestCase(8, "8 лет")]
+        [TestCase(9, "9 лет")]
+        [TestCase(10, "10 лет")]
+        [TestCase(11, "11 лет")]
+        [TestCase(12, "12 лет")]
+        [TestCase(13, "13 лет")]
+        [TestCase(14, "14 лет")]
+        [TestCase(15, "15 лет")]
+        [TestCase(16, "16 лет")]
+        [TestCase(17, "17 лет")]
+        [TestCase(18, "18 лет")]
+        [TestCase(19, "19 лет")]
+        [TestCase(20, "20 лет")]
+        [TestCase(21, "21 год")]
+        [TestCase(22, "22 года")]
+        [TestCase(23, "23 года")]
+        [TestCase(24, "24 года")]
+        [TestCase(25, "25 лет")]
+        [TestCase(30, "30 лет")]
+        [TestCase(33, "33 года")]
+        [TestCase(35, "35 лет")]
+        [TestCase(49, "49 лет")]
+        [TestCase(50, "юбилей: 50 лет")]
+        [TestCase(51, "51 год")]
+        [TestCase(55, "юбилей: 55 лет")]
+        [TestCase(80, "юбилей: 80 лет")]
+        [TestCase(81, "81 год")]
+        [TestCase(100, "юбилей: 100 лет")]
+        [TestCase(101, "101 год")]
+        [TestCase(102, "102 года")]
+        [TestCase(103, "103 года")]
+        [TestCase(105, "юбилей: 105 лет")]
+        [TestCase(205, "юбилей: 205 лет")]
+        [TestCase(301, "301 год")]
         public void GetAge_SomeYears_ReturnsText(int year, string exp)
         {
             app.YearNow = year * 2;
@@ -517,11 +560,21 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("Ivan", "0.01.1990")][TestCase("Ivan", "01.1.1990")][TestCase("Ivan", "01.01.199")]
-        [TestCase("Ivan", "01.01")][TestCase("Ivan", "01.")][TestCase("Ivan", ".01.1990")]
-        [TestCase("Ivan", "01.f.1990")][TestCase("Ivan", "01..1990")][TestCase("Ivan", "01.01.")]
-        [TestCase("Ivan", "")][TestCase("Ivan", "01011990")][TestCase("Ivan", "01,01,1990")]
-        [TestCase("Ivan", "1.1.1990")][TestCase("Ivan", "f")][TestCase("Ivan","..")]
+        [TestCase("Ivan", "0.01.1990")]
+        [TestCase("Ivan", "01.1.1990")]
+        [TestCase("Ivan", "01.01.199")]
+        [TestCase("Ivan", "01.01")]
+        [TestCase("Ivan", "01.")]
+        [TestCase("Ivan", ".01.1990")]
+        [TestCase("Ivan", "01.f.1990")]
+        [TestCase("Ivan", "01..1990")]
+        [TestCase("Ivan", "01.01.")]
+        [TestCase("Ivan", "")]
+        [TestCase("Ivan", "01011990")]
+        [TestCase("Ivan", "01,01,1990")]
+        [TestCase("Ivan", "1.1.1990")]
+        [TestCase("Ivan", "f")]
+        [TestCase("Ivan", "..")]
         public void AddPerson_BadBirthdayInputs_ThrowsException(string name, string birthday)
         {
             List<Person> people = new();
@@ -663,11 +716,22 @@ namespace happyBirthdayUnitTests
         }
 
 
-        [TestCase("Ivan", "0.01.1990")][TestCase("Ivan", "01.1.1990")][TestCase("Ivan", "01.01.199")]
-        [TestCase("Ivan", "01.01")][TestCase("Ivan", "01.")][TestCase("Ivan", ".01.1990")]
-        [TestCase("Ivan", "01.f.1990")][TestCase("Ivan", "01..1990")][TestCase("Ivan", "01.01.")]
-        [TestCase("Ivan", "")][TestCase("Ivan", "01011990")][TestCase("Ivan", "01,01,1990")]
-        [TestCase("", "01.01.1990")][TestCase("Ivan", "f")][TestCase("Ivan","..")][TestCase("","")]
+        [TestCase("Ivan", "0.01.1990")]
+        [TestCase("Ivan", "01.1.1990")]
+        [TestCase("Ivan", "01.01.199")]
+        [TestCase("Ivan", "01.01")]
+        [TestCase("Ivan", "01.")]
+        [TestCase("Ivan", ".01.1990")]
+        [TestCase("Ivan", "01.f.1990")]
+        [TestCase("Ivan", "01..1990")]
+        [TestCase("Ivan", "01.01.")]
+        [TestCase("Ivan", "")]
+        [TestCase("Ivan", "01011990")]
+        [TestCase("Ivan", "01,01,1990")]
+        [TestCase("", "01.01.1990")]
+        [TestCase("Ivan", "f")]
+        [TestCase("Ivan", "..")]
+        [TestCase("", "")]
         public void ValidInput_SomeBadValues_ReturnsFalse(string n, string bd)
         {
             bool res = app.ValidInput(n, bd);
