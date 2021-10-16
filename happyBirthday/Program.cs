c15f20467f81e82cef8e58690cbd9c7fdf84bd95
im <idevm5@ya.ru>
2021-10-16 21:46:42 +0300

    refactoring

--- happyBirthday/Program.cs
+++ happyBirthday/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace happyBirthday
 {
@@ -12,32 +13,94 @@ namespace happyBirthday
                 app.Persons = app.GetPeopleList(app.ReadFile("db.csv"));
                 //today
                 app.Text = app.GetText(app.PeopleListFilter(app.Persons));
-                app.ShowText(app.Text);
+                Console.WriteLine(app.Text);
                 //thisMonth
-                app.Text = app.GetText(app.PeopleListFilter(app.Persons, TimeMode.thisMonth), TimeMode.thisMonth);
-                app.ShowText(app.Text);
+                app.Text = app.GetText(app.PeopleListFilter(app.Persons, Mode.thisMonth), Mode.thisMonth);
+                Console.WriteLine(app.Text);
                 //thisYear
-                app.Text = app.GetText(app.PeopleListFilter(app.Persons, TimeMode.thisYear), TimeMode.thisYear);
-                app.ShowText(app.Text);
+                app.Text = app.GetText(app.PeopleListFilter(app.Persons, Mode.thisYear), Mode.thisYear);
+                Console.WriteLine(app.Text);
                 //create person or delete
                 while (true)
                 {
-                    app.ShowText($"[c]reate, [r]emove, [s]how or [q]uit? [enter]");
-                    string answer = HBApp.GetTextFromUser();
+                    Console.WriteLine($"\nHappyBirthdayApp\n[c]reate, [f]ind, [r]emove, [s]how or [q]uit? [enter]");
+                    string answer = Console.ReadLine();
                     if (answer == "c")
                     {
-                        app.Persons = app.AddPerson(app.Persons);
+                        Console.WriteLine("Введите ФИО");
+                        string name = Console.ReadLine();
+                        while (true)
+                        {
+                            if (!app.ValidInput(name: name))
+                            {
+                                Console.WriteLine("Введите корректные ФИО (например Иванов Иван Иванович):");
+                                name = Console.ReadLine().ToUpper();
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        Console.WriteLine("Введите дату рождения (в формате дд.мм.гггг):");
+                        string birthday = Console.ReadLine();
+                        while (true)
+                        {
+                            if (!app.ValidInput(birthday: birthday))
+                            {
+                                Console.WriteLine("Введите корректную дату рождения (например 02.08.1999):");
+                                birthday = Console.ReadLine();
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        app.Persons = app.AddPerson(app.Persons, name, birthday);
+                        Console.WriteLine("Успешно добавлено");
                         app.WriteFile("db.csv", app.UpdateText(app.Persons));
+                    }
+                    else if (answer == "f")
+                    {
+                        Console.WriteLine("Введите ФИО для поиска");
+                        string name = Console.ReadLine().ToUpper();
+                        app.Text = app.GetText(app.FindPerson(app.Persons, name), Mode.findResults);
+                        Console.WriteLine(app.Text);
                     }
                     else if (answer == "r")
                     {
-                        app.Persons = app.RemovePerson(app.Persons);
+                        Console.WriteLine("Введите ФИО для удаления");
+                        string name = Console.ReadLine().ToUpper();
+                        List<Person> found = app.FindPerson(app.Persons, name);
+                        if (found.Count == 0)
+                        {
+                            Console.WriteLine("Совпадений не найдено");
+                        }
+                        int ndx = 1;
+                        foreach (Person p in found)
+                        {
+                            Console.WriteLine($"Совпадение {ndx++} из {found.Count}:");
+                            Console.WriteLine($"Удалить {p.Name} ({app.ToString(p.Birthday)}.{app.ToString(p.Birthmonth)}.{p.Birthyear})? [y/n]");
+                            string answ = Console.ReadLine();
+                            while (true)
+                            {
+                                if (answ == "y")
+                                {
+                                    app.Persons = app.RemovePerson(app.Persons, p);
+                                    Console.WriteLine("Успешно удалено");
+                                    break;
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
                         app.WriteFile("db.csv", app.UpdateText(app.Persons));
                     }
                     else if (answer == "s")
                     {
-                        app.Text = app.GetText(app.PeopleListFilter(app.Persons, TimeMode.thisYear), TimeMode.thisYear);
-                        app.ShowText(app.Text);
+                        app.Text = app.GetText(app.PeopleListFilter(app.Persons, Mode.thisYear), Mode.thisYear);
+                        Console.WriteLine(app.Text);
                     }
                     else if (answer == "q")
                     {
@@ -47,15 +110,15 @@ namespace happyBirthday
             }
             catch (System.IO.FileNotFoundException ex)
             {
-                app.ShowText(ex.Message);
+                Console.WriteLine(ex.Message);
             }
             catch (FormatException ex)
             {
-                app.ShowText(ex.Message);
+                Console.WriteLine(ex.Message);
             }
             catch (ArgumentException ex)
             {
-                app.ShowText(ex.Message);
+                Console.WriteLine(ex.Message);
             }
         }
     }
